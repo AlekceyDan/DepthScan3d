a2514fa15e52e8061e017ce175a3a94347bb1e78
Alekcey Dan <danchenckov.a@yandex.ru>
2021-05-26 10:12:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 from subprocess import Popen
 from subprocess import PIPE
+import cv2
+import scan as make3d
 
-import echo.scan as model
-
+import DenseDepth.test as net
+import video
 
 from telegram import Bot
 from telegram import Update
@@ -16,7 +18,7 @@ from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from telegram.ext import CallbackQueryHandler
 #from echo.config import TG_TOKEN
-TG_TOKEN = "your token"
+TG_TOKEN = "your_token"
 
 def do_start(bot: Bot, update: Update):
     bot.send_message(
@@ -68,14 +70,29 @@ def handle_docs_photo(bot: Bot, update: Update):
 
 
 
+
+
+
         bot.send_message(chat_id= chat_id, text = "Создаю карту глубин...")
         net.depth_net()
+
+        im = cv2.imread("depth.png")
+        im = cv2.resize(im, (width,height))
+
+
+        im = im-im.min()
+        im = im/im.max()*255
+        cv2.imwrite("depth.png",im)
 
 
         if height>width:
             im = cv2.imread("depth.png")
             im = cv2.resize(im, (240, 320))
             cv2.imwrite("depth.png",im)
+
+
+
+
 
         bot.send_message(chat_id= chat_id, text = "Карта глубин готова.")
 
@@ -89,6 +106,60 @@ def handle_docs_photo(bot: Bot, update: Update):
 
     except Exception as e:
         bot.send_message(chat_id= chat_id, text = e)
+
+
+
+def handle_docs_video(bot: Bot, update: Update):
+    try:
+
+        chat_id = update.message.chat.id
+
+        file_info = bot.get_file(update.message.video.file_id)
+
+        downloaded_file = file_info.download("in.mp4")
+
+
+        #im = cv2.imread("")
+        #height, width, channels = im.shape
+
+        #im = cv2.resize(im, (640, 480))
+
+        #cv2.imwrite("test.png",im)
+
+
+
+
+        bot.send_message(chat_id= chat_id, text = "Создаю карту глубин...")
+        video.video("in.mp4")
+        video.save()
+
+        #im = cv2.imread("depth.png")
+        #im = cv2.resize(im, (width,height))
+        #cv2.imwrite("depth.png",im)
+
+        '''
+        if height>width:
+            im = cv2.imread("depth.png")
+            im = cv2.resize(im, (240, 320))
+            cv2.imwrite("depth.png",im)
+        '''
+
+        bot.send_message(chat_id= chat_id, text = "Карта глубин готова.")
+
+
+
+
+        bot.send_video(chat_id= chat_id, video = open('movie.mp4', 'rb'))
+        #bot.send_message(chat_id= chat_id, text = "Создаю 3d модель")
+        #make3d.start("depth.png")
+        #bot.send_document(chat_id=chat_id, document=open('test_3d.stl', 'rb'))
+
+    except Exception as e:
+        bot.send_message(chat_id= chat_id, text = e)
+
+
+
+
 
 def do_echo(bot: Bot, update: Update):
     chat_id = update.message.chat_id
@@ -110,12 +181,15 @@ def main():
     #message_handler = MessageHandler(Filters.text, do_echo)
     #help_handler = CommandHandler("help", do_help)
     #time_handler = CommandHandler("time", do_time)
+
     image_handler = MessageHandler(Filters.photo, handle_docs_photo)
+    video_handler = MessageHandler(Filters.video, handle_docs_video)
 
     updater.dispatcher.add_handler(start_handler)
     #updater.dispatcher.add_handler(help_handler)
     #updater.dispatcher.add_handler(time_handler)
     updater.dispatcher.add_handler(image_handler)
+    updater.dispatcher.add_handler(video_handler)
 
     #updater.dispatcher.add_handler(message_handler)
 
